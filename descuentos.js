27089bd3d28c3b3ea02bdf45ffb399102aa5b3d6
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-02-09 22:43:34 -0500

    Se conecta el archivo js con el archivo html para darle funcionalidad

--- descuentos.js
+++ descuentos.js
@@ -1,16 +1,14 @@
-const precioOriginal=120;
-const descuento=18;
-
-
 
 function calcularPrecioConDescuento(precio, descuento){
 const porcentajePrecioDescuento=100-descuento;
 const precioConDescuento=(precio*porcentajePrecioDescuento)/100;
 return precioConDescuento;
 }
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioDescuento,
-//     precioConDescuento
-// })
+function calculate_precio(){
+    var price_product=Number((document.getElementById("price_product")).value);
+    var discount=Number((document.getElementById("discount")).value);
+    var result=(calcularPrecioConDescuento(price_product,discount)).toFixed(2);
+    var resultados=document.getElementById("resultados");
+    resultados.innerHTML="Su valor a pagar es: $"+result;
+
+}
